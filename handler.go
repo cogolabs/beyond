cc25a18e7ed8f1b82b76169cbc331071684f2f3e
Joe <presbrey@gmail.com>
2017-06-08 13:01:52 -0400

    whitelist assets

--- handler.go
+++ handler.go
@@ -65,7 +65,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if r.Method == "OPTIONS" || email != "" {
+	if email != "" || whitelistHost[r.Host] {
 		if r.Header.Get("Upgrade") == "websocket" {
 			hostWS[r.Host].ServeHTTP(w, r)
 		} else {

01563e23036c11a393a9d13066ffdefe024009a0
Joe <presbrey@gmail.com>
2017-09-07 00:11:09 -0400

    add ACL error test

--- acl_test.go
+++ acl_test.go
@@ -9,17 +9,35 @@ import (
 )
 
 func init() {
-	cwd, _ := os.Getwd()
-	*fenceURL = "file://" + cwd + "/example/fence.json"
-	*sitesURL = "file://" + cwd + "/example/sites.json"
-	*whitelistURL = "file://" + cwd + "/example/whitelist.json"
-
 	t := &http.Transport{}
 	t.RegisterProtocol("file", http.NewFileTransport(http.Dir("/")))
 	httpACL.Transport = t
 }
 
-func TestACLSetup(t *testing.T) {
+const (
+	aclErrorBase   = "http://localhost:99"
+	aclErrorString = "Get " + aclErrorBase + ": dial tcp [::1]:99: getsockopt: connection refused"
+)
+
+func TestACL(t *testing.T) {
+	*fenceURL = aclErrorBase
+	*sitesURL = aclErrorBase
+	*whitelistURL = aclErrorBase
+	assert.EqualError(t, refreshFence(), aclErrorString)
+	assert.EqualError(t, refreshSites(), aclErrorString)
+	assert.EqualError(t, refreshWhitelist(), aclErrorString)
+
+	cwd, _ := os.Getwd()
+	*fenceURL = "file://" + cwd + "/example/error.json"
+	*sitesURL = "file://" + cwd + "/example/error.json"
+	*whitelistURL = "file://" + cwd + "/example/error.json"
+	assert.EqualError(t, refreshFence(), "unexpected EOF")
+	assert.EqualError(t, refreshSites(), "unexpected EOF")
+	assert.EqualError(t, refreshWhitelist(), "unexpected EOF")
+
+	*fenceURL = "file://" + cwd + "/example/fence.json"
+	*sitesURL = "file://" + cwd + "/example/sites.json"
+	*whitelistURL = "file://" + cwd + "/example/whitelist.json"
 	assert.NoError(t, setup())
 
 	assert.NotEmpty(t, fence.m)

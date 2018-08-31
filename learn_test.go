8ed1eaaecf1f6d1bf5623ea6ae48f5be5ef4e67d
Joe <presbrey@gmail.com>
2018-08-31 16:39:17 -0400

    more learning coverage

--- learn_test.go
+++ learn_test.go
@@ -7,5 +7,12 @@ import (
 )
 
 func TestLearnHostScheme(t *testing.T) {
+	ports := *learnHTTPSPorts
+	*learnHTTPSPorts = ""
+
+	assert.Equal(t, "http://neverssl.com", learnHostScheme("neverssl.com"))
+
+	*learnHTTPSPorts = ports
 	assert.Equal(t, "https://golang.org", learnHostScheme("golang.org"))
+	assert.NotNil(t, learn("golang.org"))
 }

3df13bfc4efb318deed5dd2556d4b61b812232f9
presbrey <presbrey@gmail.com>
2020-07-19 19:57:13 -0400

    add elasticsearch emitter

--- log.go
+++ log.go
@@ -1,10 +1,16 @@
 package beyond
 
 import (
+	"context"
+	"crypto/sha1"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
+	"github.com/olivere/elastic"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -17,11 +23,20 @@ var (
 	logHTTP = flag.Bool("log-http", false, "log HTTP requests to stdout")
 	logJSON = flag.Bool("log-json", false, "logrus json output")
 	logXFF  = flag.Bool("log-xff", false, "include X-Forwarded-For in logs")
+
+	logElastic   = flag.String("log-elastic", "", "csv of elasticsearch servers")
+	logElasticD  = flag.Duration("log-elastic-interval", time.Second, "how often to commit bulk updates")
+	logElasticP  = flag.String("log-elastic-prefix", "beyond", "insert this on the front of elastic indexes")
+	logElasticW  = flag.Int("log-elastic-workers", 4, "bulk commit workers")
+	logElasticCh = make(chan *elastic.BulkUpdateRequest, 20480)
 )
 
 func logSetup() error {
 	if *logJSON {
 		log.SetFormatter(&log.JSONFormatter{})
+	}
+	if *logElastic != "" {
+		return logElasticSetup(*logElastic)
 	}
 	return nil
 }
@@ -39,6 +54,7 @@ func logRoundtrip(resp *http.Response) {
 		"host":   resp.Request.Host,
 		"path":   resp.Request.URL.Path,
 		"query":  resp.Request.URL.RawQuery,
+		"origin": resp.Request.Header.Get("Origin"),
 
 		"code":     resp.StatusCode,
 		"len":      resp.ContentLength,
@@ -59,5 +75,62 @@ func logRoundtrip(resp *http.Response) {
 		}
 	}
 
-	WithFields(d).Info("HTTP")
+	if *logHTTP {
+		WithFields(d).Info("HTTP")
+	}
+	if *logElastic != "" {
+		raw, _ := json.Marshal(d)
+		hash := sha1.New()
+		hash.Write(raw)
+		id := fmt.Sprintf("%x:%x", time.Now().Unix(), hash.Sum(nil))
+		elt := elastic.NewBulkUpdateRequest().Index(*logElasticP + "-" + id[:4]).Id(id).Type("http").Doc(d).DocAsUpsert(true)
+		logElasticPut(elt, logElasticCh)
+	}
+}
+
+func logElasticPut(elt *elastic.BulkUpdateRequest, sink chan *elastic.BulkUpdateRequest) {
+	select {
+	case sink <- elt:
+	default:
+		log.Println("overflow:", elt)
+	}
+}
+
+func logElasticSetup(elasticURLs string) error {
+	elasticSearch, err := elastic.NewSimpleClient(elastic.SetURL(strings.Split(elasticURLs, ",")...))
+	if err == nil {
+		for i := 0; i < *logElasticW; i++ {
+			go func() {
+				bulk := elasticSearch.Bulk()
+				logElasticWorker(bulk, time.NewTicker(*logElasticD))
+			}()
+		}
+	}
+	return nil
+}
+
+func logElasticWorker(bulk *elastic.BulkService, tick *time.Ticker) {
+	for {
+		// if bulk.NumberOfActions() > ...
+		select {
+		case elt := <-logElasticCh:
+			bulk.Add(elt)
+
+		case <-tick.C:
+			if bulk.NumberOfActions() < 1 {
+				continue
+			}
+			r, err := bulk.Do(context.Background())
+			if err != nil {
+				log.Println(err)
+			}
+			if r == nil {
+				continue
+			}
+			fails := r.Failed()
+			if len(fails) > 0 {
+				log.Println(len(fails), fails[0].Error)
+			}
+		}
+	}
 }
--- log_test.go
+++ log_test.go
@@ -1,15 +1,57 @@
 package beyond
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/olivere/elastic"
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	logElasticTestErrorN = 0
+	logElasticTestServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/_bulk":
+			rBody, _ := ioutil.ReadAll(r.Body)
+			switch {
+			case strings.Contains(string(rBody), "ERROR") && logElasticTestErrorN < 5:
+				logElasticTestErrorN++
+				w.WriteHeader(500)
+				fmt.Fprint(w, `{`)
+			case strings.Contains(string(rBody), "FAILED"):
+				w.WriteHeader(200)
+				r := &mockElasticBulkUpdateResponse{}
+				r.Items = append(r.Items, mockElasticBulkUpdateResponseItem{})
+				r.Items[0].Update.Shards.Failed = 10
+				json.NewEncoder(w).Encode(r)
+			default:
+				w.WriteHeader(200)
+				fmt.Fprint(w, `{}`)
+			}
+		default:
+			w.WriteHeader(204)
+			log.Printf("ESTEST(%s): %+v\n", r.URL.Path, r)
+		}
+	}))
 )
 
 func init() {
 	*logJSON = true
 	*logXFF = true
+
+	*logElastic = ""
+	logSetup() // cover nil URL
+	*logElastic = logElasticTestServer.URL
+	*logElasticD = time.Millisecond
+	*logElasticW = 1
 }
 
 func TestLogHTTP(t *testing.T) {
@@ -19,4 +61,41 @@ func TestLogHTTP(t *testing.T) {
 	assert.NoError(t, err)
 	resp := &http.Response{Request: req}
 	logRoundtrip(resp)
+}
+
+func TestLogElasticOverflow(t *testing.T) {
+	elt := elastic.NewBulkUpdateRequest()
+	ch := make(chan *elastic.BulkUpdateRequest)
+	logElasticPut(elt, ch)
+}
+
+func TestLogElasticWorker(t *testing.T) {
+	time.Sleep(10 * time.Millisecond)
+	logElasticCh <- elastic.NewBulkUpdateRequest().Index("FAILED")
+	time.Sleep(10 * time.Millisecond)
+	logElasticCh <- elastic.NewBulkUpdateRequest().Index("ERROR")
+	time.Sleep(10 * time.Millisecond)
+}
+
+type mockElasticBulkUpdateResponse struct {
+	Took   int                                 `json:"took"`
+	Errors bool                                `json:"errors"`
+	Items  []mockElasticBulkUpdateResponseItem `json:"items"`
+}
+
+type mockElasticBulkUpdateResponseItem struct {
+	Update struct {
+		Index   string `json:"_index"`
+		ID      string `json:"_id"`
+		Version int    `json:"_version"`
+		Result  string `json:"result"`
+		Shards  struct {
+			Total      int `json:"total"`
+			Successful int `json:"successful"`
+			Failed     int `json:"failed"`
+		} `json:"_shards"`
+		SeqNo       int `json:"_seq_no"`
+		PrimaryTerm int `json:"_primary_term"`
+		Status      int `json:"status"`
+	} `json:"update"`
 }

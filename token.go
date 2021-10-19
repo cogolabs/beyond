861053e29705df46fe53b83493099f110d33a3d5
presbrey <presbrey@gmail.com>
2021-10-19 14:47:27 -0400

    fixup GitHub bearer tokens

--- token.go
+++ token.go
@@ -11,8 +11,7 @@ import (
 )
 
 var (
-	tokenBase = flag.String("token-base", "", "token server URL prefix (eg. https://api.github.com/user?access_token=)")
-	tokenPost = flag.Bool("token-post", true, "POST token as Bearer instead of query string")
+	tokenBase = flag.String("token-base", "", "token server URL prefix (eg. https://api.github.com/user)")
 
 	tokenCache = cache.New(10*time.Minute, 10*time.Minute)
 
@@ -49,26 +48,19 @@ func tokenAuth(r *http.Request) string {
 			return v
 		}
 	}
-	var err error
-	var resp *http.Response
-	if *tokenPost {
-		req, err := http.NewRequest("POST", *tokenBase, nil)
-		if err != nil {
-			Error(err)
-			return ""
-		}
-		req.Header.Set("Authorization", "Bearer "+token)
-		resp, err = http.DefaultClient.Do(req)
-		if err != nil {
-			Error(err)
-			return ""
-		}
-	} else {
-		resp, err = http.Get(*tokenBase + token)
-		if err != nil {
-			Error(err)
-			return ""
-		}
+
+	url := *tokenBase
+	method := "GET"
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		Error(err)
+		return ""
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		Error(err)
+		return ""
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
